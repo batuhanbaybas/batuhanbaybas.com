b7c6098a9e2f4d6c6fad55dea83a9e16dce3be9d
batuhanbaybas <b.baybas@gmail.com>
2023-12-07 12:31:27 +0300

    Refactor code and remove unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import "./styles/index.scss";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {}
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<RouterProvider router={router} />);
+root.render(<Home />);
