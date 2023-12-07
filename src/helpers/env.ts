0aba1cb1b8f1f0e88bde852d80c27618ef1e7656
batuhanbaybas <b.baybas@gmail.com>
2023-12-07 12:35:38 +0300

    Delete axios-instance.ts, env.ts, and utils.ts

--- src/helpers/env.ts
+++
@@ -1,3 +0,0 @@
-export const env = {
-  baseURL: process.env.REACT_APP_API_BASE_URL
-};
--- src/helpers/utils.ts
+++
@@ -1,3 +0,0 @@
-export const trancate = (text: string, len: number = 250) => {
-  return text?.length > len ? text.substring(0, len - 1) + "..." : text;
-};
